caa61df15f57051e88de86cc20a9bf22e43ddfe2
Res Yeg <shaqk702@gmail.com>
2018-11-11 16:51:51 -0800

    finish playing with bootstrap and lawton themes

--- tutorial-part-two/src/utils/typography.js
+++ tutorial-part-two/src/utils/typography.js
@@ -1,5 +1,9 @@
 import Typography from "typography"
+// import bootstrapTheme from "typography-theme-bootstrap"
+import lawtonTheme from "typography-theme-lawton"
 
-const typography = new Typography({ baseFontSize: "18px" })
+// const typography = new Typography({ baseFontSize: "18px" })
+// const typography = new Typography(bootstrapTheme)
+const typography = new Typography(lawtonTheme)
 
 export default typography
