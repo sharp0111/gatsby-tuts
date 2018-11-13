7ffd778e3eccb002c91b14581879d25a0beb92f0
Res Yeg <shaqk702@gmail.com>
2018-11-12 23:45:48 -0800

    create slugs

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,6 +1,16 @@
-exports.onCreateNode = ({ node, getNode }) => {
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
     if(node.internal.type === `MarkdownRemark`) {
-        const fileNode = getNode(node.parent)
-        console.log(`\n`, fileNode.relativePath)
+        // const fileNode = getNode(node.parent)
+        // console.log(`\n`, fileNode.relativePath)
+        // console.log(createFilePath({ node, getNode, basePath: `pages` }))
+        const slug = createFilePath({ node, getNode, basePath: `pages` })
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        })
     }
 }
